9bd2b17438640124e12675ecd33731204fd89926
unknown <sanchit2005yudavanshi@gmail.com>
2023-12-06 21:40:53 +0530

    connected the server to mongodb atlas

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+
+app.get('/', (req,res)=>{
+    res.status(200).json({message: "working"})
+})
+
+
+mongoose.connect(process.env.DB_URL)
+    .then(()=>{
+        app.listen(process.env.PORT, ()=>{
+            console.log("connected to server and atlas on port:- ", process.env.PORT)
+        })
+    })
+    .catch((error)=>{
+        console.log(error)
+    })

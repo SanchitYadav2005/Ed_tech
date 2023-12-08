767a024265a0982a186b31681909cf825e547472
unknown <sanchit2005yudavanshi@gmail.com>
2023-12-08 21:55:12 +0530

    created controller for login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,17 +3,19 @@ const User = require('../schemas/useSchema')
 module.exports.signup = async(req,res)=>{
     const {email, password} = req.body;
     try{
-        const user = await User.create({email, password});
+        const user = await User.signup(email, password);
         res.status(200).json({message:"created a user", user})
     }catch(error){
         res.status(401).json({message: error.message})
     }
 }
 
-// module.exports.login = async(req,res)=>{
-//     const {email,password} = req.body;
-//     try{
-//         const user = await User.findOne({email, password})
-
-//     }
-// }
+module.exports.login = async(req,res)=>{
+    const {email,password} = req.body;
+    try{
+        const user = await User.login({email, password})
+        res.status(200).json({message: "logged in successfully", user})
+    }catch(error){
+        res.status(401).json({message: error.message})
+    }
+}

b18024406a243b6c6afe3d0b63371a36a5fc73c8
unknown <sanchit2005yudavanshi@gmail.com>
2023-12-23 08:28:50 +0530

    navbar done

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,17 +1,34 @@
-import '../styles/navbar.scss'
-import logoImg from '../assets/logo.png'
+import "../styles/navbar.scss";
+import logoImg from "../assets/logo.png";
 
-function Navbar(){
-    return(
-        <nav className="navbar">
-            <img src={logoImg} alt="logo-display"/>
-            <ul className="nav">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Contact</a></li>
-            </ul>
-        </nav>
-    )
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <img src={logoImg} className="logo-img" alt="logo-display" />
+      <ul className="nav">
+        <li className="nav-item">
+          <a className="nav-link" href="/">
+            Home
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/about">
+            About
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/contact">
+            Contact
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="/contact">
+            Notes
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;

23fc6b3eb3c5c90f59616fee77e6f48fe422a96a
unknown <sanchit2005yudavanshi@gmail.com>
2023-12-26 21:43:10 +0530

    working on footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,7 +4,17 @@ const Footer = () => {
     return ( 
         <>
             <Footer>
-                
+                <div>
+                    <ul>
+                        <li><a href="/contact">Contact</a></li>
+                        <li><a href="https://dot.gov.in/network-status">Network Status</a></li>
+                        <li><a href="https://www.random.org/faq/">FAQ</a></li>
+                    </ul>
+                </div>
+                <form>
+                    <input type='email' id='email' placeholder="Enter email address" />
+                    <button>Send</button>
+                </form>
             </Footer>
         </>
      );

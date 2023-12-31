5e5b31246384b58e01113015bb6a753d65995eb2
unknown <sanchit2005yudavanshi@gmail.com>
2023-12-31 11:28:29 +0530

    done

--- backend/routes/staticRoutes.js
+++ backend/routes/staticRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router();
 const {staticControl} = require('../controllers/staticController')
 
-router.get('', staticControl)
+router.get('/', staticControl)
 
 
 module.exports = router;

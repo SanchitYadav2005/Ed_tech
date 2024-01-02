e4572259fc91e767e1af58499139a47e23dbeefc
unknown <sanchit2005yudavanshi@gmail.com>
2024-01-02 13:22:21 +0530

    added login and sign up page to the routes '

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,7 @@
 import Home from "./pages/Home";
 import Diversion from "./pages/Diversion";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -8,6 +10,10 @@ function App() {
       <Routes>
         <Route path="/" element={ <Home />}/>
         <Route path="/register" element={<Diversion />} />
+        <Route path="/developer/login" element={<Login/>}/>
+        <Route path="/learner/login" element={<Login/>}/>
+        <Route path="/developer/signup" element={<Signup/>}/>
+        <Route path="/learner/signup" element={<Signup/>}/>
       </Routes>
      
     </>

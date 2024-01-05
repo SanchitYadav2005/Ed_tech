07bc2065c61d0e827d89a03e27eeae9e9880f478
unknown <sanchit2005yudavanshi@gmail.com>
2024-01-05 20:05:57 +0530

    used the class to send error when an unusual route hits

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,40 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const path = require('path')
+const path = require("path");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const userRoutes = require("./routes/userRoutes");
-const staticRoutes = require('./routes/staticRoutes')
+const staticRoutes = require("./routes/staticRoutes");
 const session = require("express-session");
+const ExpressError = require("./utils/ExpressError");
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(session({
-  secret: process.env.SECRET,
-  resave: false,
-  saveUninitialized: true,
-  cookie: {secure: true}
-}))
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      httpOnly: true,
+      secure: true,
+      expires: Date.now + 1000 * 60 * 60 * 24 * 7,
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    },
+  })
+);
 
 //routes
 app.use("/api/user/", userRoutes);
 app.use("/", staticRoutes);
+
+app.all("*", (req, res, next) => {
+  next(new ExpressError("page not found", 404))
+});
 
 mongoose
   .connect(process.env.DB_URL)

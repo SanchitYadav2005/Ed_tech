fe3057a6cd8184a2e18697ece863fc9227d0413b
unknown <sanchit2005yudavanshi@gmail.com>
2024-01-05 15:40:03 +0530

    sended data using props

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,19 +3,31 @@ import Diversion from "./pages/Diversion";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
+  const [isDeveloper, toggleDeveloper] = useState(true);
+
+  const toggle = () => {
+    toggleDeveloper((prevState) => !prevState);
+  };
+
   return (
     <>
       <Routes>
-        <Route path="/" element={ <Home />}/>
-        <Route path="/register" element={<Diversion />} />
-        <Route path="/developer/login" element={<Login/>}/>
-        <Route path="/learner/login" element={<Login/>}/>
-        <Route path="/developer/signup" element={<Signup/>}/>
-        <Route path="/learner/signup" element={<Signup/>}/>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/register"
+          element={<Diversion toggleDeveloper={toggle} />}
+        />
+        <Route path="/developer/login" element={<Login />} />
+        <Route path="/learner/login" element={<Login />} />
+        <Route
+          path="/developer/signup"
+          element={<Signup isDeveloper={isDeveloper} />}
+        />
+        <Route path="/learner/signup" element={<Signup />} />
       </Routes>
-     
     </>
   );
 }

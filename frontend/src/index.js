3d96eff188ee559f9a2dac82b141c48451b20240
unknown <sanchit2005yudavanshi@gmail.com>
2024-01-10 19:27:09 +0530

    wrapped the app js with the context

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,15 +4,18 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import {BrowserRouter} from "react-router-dom";
+import { AuthContextProvider } from "./context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 
+  <React.StrictMode>
   <BrowserRouter>
-    <React.StrictMode>
+    <AuthContextProvider>
       <App />
-    </React.StrictMode>
+      </AuthContextProvider>
   </BrowserRouter>
+  </React.StrictMode>
 
 );
 

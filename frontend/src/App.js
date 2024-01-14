73dfbb6ebfd320992c1e71c100204b65869c36f2
unknown <sanchit2005yudavanshi@gmail.com>
2024-01-14 12:26:40 +0530

    working on redirecting after login and sign up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,23 @@
+import React from "react";
 import Home from "./pages/Home";
 import Diversion from "./pages/Diversion";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import FilePage from "./pages/FilePage";
 import { Routes, Route } from "react-router-dom";
 import { useState } from "react";
+import useAuthContext from "./hooks/useAuthContext";
 
 function App() {
   const [isDeveloper, toggleDeveloper] = useState(true);
+  const { user } = useAuthContext();
 
   const toggle = () => {
     toggleDeveloper((prevState) => !prevState);
   };
 
   return (
-    <>
+    <React.Fragment>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route
@@ -30,8 +34,12 @@ function App() {
           element={<Signup isDeveloper={isDeveloper} />}
         />
         <Route path="/learner/signup" element={<Signup />} />
+        <Route
+          path={`/developer/${user?.developer?._id}/`}
+          element={<FilePage />}
+        />
       </Routes>
-    </>
+    </React.Fragment>
   );
 }
 

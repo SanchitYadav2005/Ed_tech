aa1629b00914ca48f1675271fccee83ff08c6070
unknown <sanchit2005yudavanshi@gmail.com>
2024-01-19 18:36:04 +0530

    working on file page

--- frontend/src/pages/FilePage.js
+++ frontend/src/pages/FilePage.js
@@ -1,13 +1,19 @@
 import SecondNavbar from "../components/SecondNavbar";
+import "../styles/filePage.scss";
 
 const FilePage = () => {
   return (
     <>
       <SecondNavbar />
-      <main>
-        <input type="file" className="file"/>
-        <div>
-            <h3>You don't have any uploaded files yet</h3>
+      <main className="main-container">
+        <form>
+          <label className="file-label" for="file">
+            Choose file
+          </label>
+          <input type="file" className="file" id="file" />
+        </form>
+        <div className="uploaded-files-container">
+          <h3>You don't have any uploaded files yet</h3>
         </div>
       </main>
     </>

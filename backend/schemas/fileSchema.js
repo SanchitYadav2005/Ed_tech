79d68c850d1d836926325a37ee537b4505ff1b8b
unknown <sanchit2005yudavanshi@gmail.com>
2024-01-20 20:42:58 +0530

    created schema and validator for the file that will be uploaded

--- backend/schemas/fileSchema.js
+++ backend/schemas/fileSchema.js
@@ -1,2 +1,16 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const fileSchema = new Schema({
+  file: {
+    type: Buffer,
+    validate: {
+      validator: function (value) {
+        return value.slice(0.5).toString("utf-8") === "%PDF-";
+      },
+      message: (props) => `${props.value} is not a valid PDF fiile`,
+    },
+  },
+});
+
+module.exports = mongoose.model("File", fileSchema);

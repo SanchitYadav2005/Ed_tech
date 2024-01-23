90fec82914c71732afe69339a20c3de1bf492a41
unknown <sanchit2005yudavanshi@gmail.com>
2024-01-23 13:34:21 +0530

    rewrites the logout hook

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,11 +1,13 @@
 import useAuthContext from "./useAuthContext";
 
-export const useLogout = () => {
+export const useLogOut = () => {
   const { dispatch } = useAuthContext();
 
   const logout = () => {
     localStorage.removeItem("user");
+    localStorage.removeItem("token");
     dispatch({ type: "LOGOUT" });
+    console.log("user logged out successfully")
   };
   return { logout };
 };

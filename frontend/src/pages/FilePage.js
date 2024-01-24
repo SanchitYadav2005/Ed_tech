81ba6e46092312e40e65c1f65dfc3a14e768e9a4
unknown <sanchit2005yudavanshi@gmail.com>
2024-01-24 12:50:44 +0530

    worked on displaying file name

--- frontend/src/pages/FilePage.js
+++ frontend/src/pages/FilePage.js
@@ -1,17 +1,39 @@
+import React, { useState, useEffect } from "react";
 import SecondNavbar from "../components/SecondNavbar";
 import "../styles/filePage.scss";
 
 const FilePage = () => {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [selectedFileName, setSelectedFileName] = useState("");
+
+  const handleChange = (e) => {
+    const file = e.target.files[0];
+    setSelectedFile(file);
+    setSelectedFileName(file ? file.name : ""); // Set the file name if a file is selected
+  };
+
+  useEffect(() => {
+    console.log(selectedFile);
+  }, [selectedFile]);
+
   return (
     <>
       <SecondNavbar />
       <main className="main-container">
         <form className="file-container">
-          <label className="file-label" for="file">
+          <label className="file-label" htmlFor="file">
             Choose file
           </label>
-          <input type="file" className="file" id="file" />
+          <input
+            type="file"
+            className="file"
+            id="file"
+            onChange={handleChange}
+          />
         </form>
+        
+        <button className="upload-btn">Upload</button>
+        <p className="selected-file">Selected File: <span className="span">{selectedFileName}</span></p>
         <div className="uploaded-files-container">
           <h3>You don't have any uploaded files yet</h3>
         </div>

283c4715e400e1bddc979da5769243c58725a6e8
unknown <sanchit2005yudavanshi@gmail.com>
2024-02-03 12:49:18 +0530

    seted route for getting user

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,11 +5,13 @@ const {
   DeveloperLogin,
   LearnerLogin,
   LearnerSignup,
+  getUserById
 } = require("../controllers/userController");
 
 router.post("/developer/signup", DeveloperSignup);
 router.post("/developer/login", DeveloperLogin);
 router.post("/learner/signup", LearnerSignup);
 router.post("/learner/login", LearnerLogin);
+router.get("/:id/dashboard", getUserById)
 
 module.exports = router;

d109fb17fec931388aad14a4ee5afc82fd327bad
unknown <sanchit2005yudavanshi@gmail.com>
2024-02-03 16:50:31 +0530

    the files will be automatically deleted if the user delete his account

--- backend/schemas/fileSchema.js
+++ backend/schemas/fileSchema.js
@@ -14,4 +14,14 @@ const fileSchema = new Schema({
   author: [{ type: Schema.Types.ObjectId, ref: "Developer" }],
 });
 
+fileSchema.post('findByIdAndDelete', async(doc)=>{
+  if(doc){
+      await Review.deleteMany({
+          _id: {
+              $in: doc.review
+          }
+      })
+  }
+})
+
 module.exports = mongoose.model("File", fileSchema);

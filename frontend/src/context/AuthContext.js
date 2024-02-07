65e7a182fc7feff5727ddbeac67b6c955dd1067e
unknown <sanchit2005yudavanshi@gmail.com>
2024-02-07 20:24:31 +0530

    rewritten the context

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,5 +1,5 @@
 import { useReducer, createContext, useEffect } from "react";
-import { authReducer } from "../reducers/authReducer";
+import {authReducer} from "../reducers/authReducer";
 
 export const AuthContext = createContext();
 
@@ -7,27 +7,24 @@ export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(authReducer, {
     user: null,
   });
-
   useEffect(() => {
     const storedUser = JSON.parse(localStorage.getItem("user"));
     if (storedUser) {
       dispatch({ type: "LOGIN", payload: storedUser });
     }
-    console.log(storedUser)
   }, []);
-
-  useEffect(() => {
-    console.log("AuthContext state:", state);
-  }, [state]);
 
   const contextValue = {
     state,
     dispatch,
   };
+  console.log("AuthContext state: ", state);
 
   return (
-    <AuthContext.Provider value={contextValue}>
-      {children}
-    </AuthContext.Provider>
+    <>
+      <AuthContext.Provider value={{state,dispatch}}>
+        {children}
+      </AuthContext.Provider>
+    </>
   );
 };

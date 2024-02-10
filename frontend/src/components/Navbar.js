68956548be74162f17b2ac8e743e79f810d4947b
unknown <sanchit2005yudavanshi@gmail.com>
2024-02-10 15:28:46 +0530

    alligned the navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,27 +4,29 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <>
-    <nav className="navbar">
-      <h1 className="rainbow-text">DEZIRE</h1>
-      <ul className="nav">
-        <li className="nav-item">
-          <a className="nav-link" href="/">
-            Home
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/contact">
-            Contact
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="/notes">
-            Notes
-          </a>
-        </li>
-      </ul>
-      <Link to="/register" className="btn">Register</Link>
-    </nav>
+      <nav className="navbar">
+        <h1 className="rainbow-text">DEZIRE</h1>
+        <ul className="nav">
+          <li className="nav-item">
+            <a className="nav-link" href="/">
+              Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/contact">
+              Contact
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/notes">
+              Notes
+            </a>
+          </li>
+        </ul>
+        <Link to="/register" className="btn">
+          Register
+        </Link>
+      </nav>
     </>
   );
 }

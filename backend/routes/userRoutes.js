9c7f11b8d83dd7b5d6b4c3cb065a88303441bf80
unknown <sanchit2005yudavanshi@gmail.com>
2024-02-12 21:21:11 +0530

    fixed route for :id

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -16,7 +16,7 @@ router.post("/developer/login", DeveloperLogin);
 router.post("/learner/signup", LearnerSignup);
 router.post("/learner/login", LearnerLogin);
 router.get("/:id/dashboard", getUserById);
-router.put("/develope/:id/dashboard", updateDeveloper);
+router.put("/developer/:id/dashboard", updateDeveloper);
 router.put("/learner/:id/dashboard", updateLearner);
 router.post("/:id/delete", deleteUser)
 

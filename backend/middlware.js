ab85609bb981950d08882975048ca64cfdd7ebdb
unknown <sanchit2005yudavanshi@gmail.com>
2024-02-24 21:35:13 +0530

    deleted the middleware file as there is no need of it

--- backend/middlware.js
+++
@@ -1,8 +0,0 @@
-module.exports.setDeveloperId = (req,res,next)=>{
-    if(req.user && req.user._id){
-        req.developer = {_id: req.user._id}
-    }else{
-        req.developer = null
-    }
-    next()
-}

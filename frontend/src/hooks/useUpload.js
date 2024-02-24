9349951acc89b4ce1473b2303ec7fd532be40c92
unknown <sanchit2005yudavanshi@gmail.com>
2024-02-24 21:35:56 +0530

    updated the url for uploading the file and added localstroage fetching to get the user id to upload the file

--- frontend/src/hooks/useUpload.js
+++ frontend/src/hooks/useUpload.js
@@ -1,14 +1,27 @@
 import axios from "axios";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export const useUpload = () => {
   const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const getData = () => {
+      const res = localStorage.getItem("user");
+      if (res) {
+        const jsonRes = JSON.parse(res);
+        setData(jsonRes);
+      } else {
+        console.log("can not get data from local storage!");
+      }
+    };
+    getData();
+  }, []);
 
   const upload = async (selectedFile) => {
     const formData = new FormData();
     formData.append("file", selectedFile);
 
-    
     setIsLoading(!isLoading);
 
     const config = {
@@ -19,15 +32,15 @@ export const useUpload = () => {
 
     try {
       const response = await axios.post(
-        "http://localhost:8080/api/user/developer/file",
+        `http://localhost:8080/api/user/developer/${data.developer._id}/file`,
         formData,
         config
       );
       console.log("File upload successful:", response.data);
-      console.log(isLoading)
+      console.log(isLoading);
     } catch (error) {
       console.error("Error uploading file:", error);
-    } 
+    }
   };
 
   return { upload, isLoading };

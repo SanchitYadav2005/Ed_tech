53936bd241485d0d45deb6e97ea7405bec0498b0
unknown <sanchit2005yudavanshi@gmail.com>
2024-02-27 21:22:14 +0530

    added multer method in the route

--- backend/routes/fileRoutes.js
+++ backend/routes/fileRoutes.js
@@ -6,8 +6,10 @@ const {
   getSingleFile,
   deleteFile,
 } = require("../controllers/fileController");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
-router.post("/developer/:id/file", uploadFile);
+router.post("/developer/:id/file", upload.single("file"), uploadFile);
 router.get("/files", getAllFiles);
 router.get("/:id/file", getSingleFile);
 router.delete("/:id/delete", deleteFile);

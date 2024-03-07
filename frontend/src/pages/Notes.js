898aa731faca2fc51e7e5e9955a7764c1d98e8ba
unknown <sanchit2005yudavanshi@gmail.com>
2024-03-07 17:31:06 +0530

    used objet to get page of file

--- frontend/src/pages/Notes.js
+++ frontend/src/pages/Notes.js
@@ -1,14 +1,29 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import SecondNavbar from "../components/SecondNavbar";
-import {Document, Page} from 'react-pdf';
-
+import pdfFile from "../assets/file.pdf";
 
 const Notes = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get("http://localhost:8080/api/user/files");
+      if (response) {
+        setData(response.data);
+      }
+    };
+    fetchData()
+  });
+
   return (
     <>
       <SecondNavbar />
-     <Document file="www.cs.cmu.edu/afs/cs.cmu.edu/user/gchen/www/download/java/LearnJava.pdf">
-      <Page pageNumber={2} />
-     </Document>
+      {/* <object data={pdfFile} width="500" height="500" aria-label="pdf file"/> */}
+    {data.map(e=>(
+<embed src={pdfFile} width="500px" height="500px" />
+    ))}
+      
     </>
   );
 };

869aed1d9f49c40f5cd641179a89141bc3a7ce13
unknown <sanchit2005yudavanshi@gmail.com>
2024-03-07 17:30:03 +0530

    removed the auth for the file '

--- backend/schemas/fileSchema.js
+++ backend/schemas/fileSchema.js
@@ -2,26 +2,18 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const fileSchema = new Schema({
-  file: {
-    type: Buffer,
-    validate: {
-      validator: function (value) {
-        return value.slice(0.5).toString("utf-8") === "%PDF-";
-      },
-      message: (props) => `${props.value} is not a valid PDF fiile`,
-    },
-  },
+  file: {},
   author: { type: Schema.Types.ObjectId, ref: "Developer" },
 });
 
-fileSchema.post('findByIdAndDelete', async(doc)=>{
-  if(doc){
-      await Review.deleteMany({
-          _id: {
-              $in: doc.review
-          }
-      })
+fileSchema.post("findByIdAndDelete", async (doc) => {
+  if (doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.review,
+      },
+    });
   }
-})
+});
 
 module.exports = mongoose.model("File", fileSchema);

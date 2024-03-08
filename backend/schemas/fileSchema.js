a5f55abe138b40966a12926197ec8b11a3e11337
unknown <sanchit2005yudavanshi@gmail.com>
2024-03-08 22:12:16 +0530

    updated the schema

--- backend/schemas/fileSchema.js
+++ backend/schemas/fileSchema.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const fileSchema = new Schema({
-  file: {},
+  file: {
+    type: String,
+  },
   author: { type: Schema.Types.ObjectId, ref: "Developer" },
 });
 

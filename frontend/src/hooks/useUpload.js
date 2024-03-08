4d8369550ba9dd15150c404e5628a5f7575aaf21
unknown <sanchit2005yudavanshi@gmail.com>
2024-03-08 22:11:27 +0530

    working on api post to work fine to transfert eh fie

--- frontend/src/hooks/useUpload.js
+++ frontend/src/hooks/useUpload.js
@@ -21,23 +21,18 @@ export const useUpload = () => {
   const upload = async (selectedFile) => {
     const formData = new FormData();
     formData.append("file", selectedFile);
+    
 
     setIsLoading((prevState) => !prevState);
 
-    const config = {
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-    };
+
 
     try {
       const response = await axios.post(
-        `http://localhost:8080/api/user/developer/${data.developer._id}/file`,
-        formData,
-        config
+        `http://localhost:8080/api/user/developer/${data.developer._id}/file`, {formData}
       );
       console.log("File upload successful:", response.data);
-      localStorage.setItem("notes", JSON.stringify(response.data))
+      localStorage.setItem("notes", JSON.stringify(response.data));
       console.log(isLoading);
     } catch (error) {
       console.error("Error uploading file:", error);

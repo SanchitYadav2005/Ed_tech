150a162c3b593e53f9ba922129fcfa8cb01fb00d
unknown <sanchit2005yudavanshi@gmail.com>
2024-03-08 22:12:53 +0530

    worked on routes

--- backend/routes/fileRoutes.js
+++ backend/routes/fileRoutes.js
@@ -7,9 +7,14 @@ const {
   deleteFile,
 } = require("../controllers/fileController");
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024 * 1024 * 1024 * 1024,
+  },
+});
 
-router.post("/developer/:id/file", upload.single('file'), uploadFile);
+router.post("/developer/:id/file", upload.single("selectedFile"), uploadFile);
 router.get("/files", getAllFiles);
 router.get("/:id/file", getSingleFile);
 router.delete("/:id/delete", deleteFile);

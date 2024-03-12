966110df62db65986d6f95bc81fc1a96373a658a
unknown <sanchit2005yudavanshi@gmail.com>
2024-03-12 21:56:03 +0530

    rewirtten the full useUpload

--- frontend/src/hooks/useUpload.js
+++ frontend/src/hooks/useUpload.js
@@ -1,48 +1,82 @@
+// import axios from "axios";
+// import { useState, useEffect } from "react";
+
+// export const useUpload = () => {
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [data, setData] = useState(null);
+
+//   useEffect(() => {
+//     const getData = () => {
+//       const res = localStorage.getItem("user");
+//       if (res) {
+//         const jsonRes = JSON.parse(res);
+//         setData(jsonRes);
+//       } else {
+//         console.log("can not get data from local storage!");
+//       }
+//     };
+//     getData();
+//   }, []);
+
+//   const upload = async (selectedFile) => {
+//     // const formData = new FormData();
+//     // formData.append("file", selectedFile);
+
+//     setIsLoading(!isLoading);
+
+//     const config = {
+//       headers: {
+//         "Content-Type": "multipart/form-data",
+//       },
+//     };
+//     try {
+//       const response = await axios.post(
+//         `http://localhost:8080/api/user/developer/${data.developer._id}/file`,
+//         selectedFile
+//       );
+//       console.log("File upload successful:", response.data);
+//       localStorage.setItem("notes", JSON.stringify(response.data));
+//       console.log(isLoading);
+//     } catch (error) {
+//       console.error("Error uploading file:", error);
+//     }
+//   };
+
+//   return { upload, isLoading };
+// };
+
 import axios from "axios";
 import { useState, useEffect } from "react";
 
 export const useUpload = () => {
   const [isLoading, setIsLoading] = useState(false);
-  const [data, setData] = useState(null);
+  const [userData, setUserData] = useState(null);
 
   useEffect(() => {
-    const getData = () => {
-      const res = localStorage.getItem("user");
-      if (res) {
-        const jsonRes = JSON.parse(res);
-        setData(jsonRes);
-      } else {
-        console.log("can not get data from local storage!");
-      }
-    };
-    getData();
+    const data = localStorage.getItem("user");
+    if (data) {
+      const jsonData = JSON.parse(data);
+      setUserData(jsonData);
+    } else {
+      console.log("cannot get data from localstorage!");
+    }
   }, []);
 
   const upload = async (selectedFile) => {
-    // const formData = new FormData();
-    // formData.append("file", selectedFile);
-    
-
-    setIsLoading(!isLoading);
-
-    const config = {
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-    };
     try {
-      const response = await axios.post(
-        `http://localhost:8080/api/user/developer/${data.developer._id}/file`,
-        selectedFile,
-        config
+      setIsLoading(!isLoading)
+      const res = await axios.post(
+        `http://localhost:8080/api/user/developer/${userData.developer._id}/file`,
+        { selectedFile }
       );
-      console.log("File upload successful:", response.data);
-      localStorage.setItem("notes", JSON.stringify(response.data));
-      console.log(isLoading);
+      if (res) {
+        console.log(res.data);
+      } else {
+        console.log("not able to post the file ");
+      }
     } catch (error) {
-      console.error("Error uploading file:", error);
+      console.log(error);
     }
   };
-
-  return { upload, isLoading };
+  return{isLoading, upload}
 };

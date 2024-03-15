747e410cc8a467c9a715a27ff534b8aee0ee3213
unknown <sanchit2005yudavanshi@gmail.com>
2024-03-15 20:46:45 +0530

    useUplaod is wokring nicely

--- frontend/src/hooks/useUpload.js
+++ frontend/src/hooks/useUpload.js
@@ -1,50 +1,3 @@
-// import axios from "axios";
-// import { useState, useEffect } from "react";
-
-// export const useUpload = () => {
-//   const [isLoading, setIsLoading] = useState(false);
-//   const [data, setData] = useState(null);
-
-//   useEffect(() => {
-//     const getData = () => {
-//       const res = localStorage.getItem("user");
-//       if (res) {
-//         const jsonRes = JSON.parse(res);
-//         setData(jsonRes);
-//       } else {
-//         console.log("can not get data from local storage!");
-//       }
-//     };
-//     getData();
-//   }, []);
-
-//   const upload = async (selectedFile) => {
-//     // const formData = new FormData();
-//     // formData.append("file", selectedFile);
-
-//     setIsLoading(!isLoading);
-
-//     const config = {
-//       headers: {
-//         "Content-Type": "multipart/form-data",
-//       },
-//     };
-//     try {
-//       const response = await axios.post(
-//         `http://localhost:8080/api/user/developer/${data.developer._id}/file`,
-//         selectedFile
-//       );
-//       console.log("File upload successful:", response.data);
-//       localStorage.setItem("notes", JSON.stringify(response.data));
-//       console.log(isLoading);
-//     } catch (error) {
-//       console.error("Error uploading file:", error);
-//     }
-//   };
-
-//   return { upload, isLoading };
-// };
-
 import axios from "axios";
 import { useState, useEffect } from "react";
 
@@ -64,19 +17,31 @@ export const useUpload = () => {
 
   const upload = async (selectedFile) => {
     try {
-      setIsLoading(!isLoading)
+      setIsLoading(true); // Set loading state to true before the request
+
+      const formData = new FormData();
+      formData.append("file", selectedFile); // Use "file" or the appropriate field name expected by the server
+
       const res = await axios.post(
         `http://localhost:8080/api/user/developer/${userData.developer._id}/file`,
-        { selectedFile }
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data", // Set proper Content-Type header
+          },
+        }
       );
+
       if (res) {
         console.log(res.data);
       } else {
-        console.log("not able to post the file ");
+        console.log("Not able to post the file");
       }
     } catch (error) {
       console.log(error);
+    } finally {
+      setIsLoading(false); // Set loading state to false regardless of success or failure
     }
   };
-  return{isLoading, upload}
+  return { isLoading, upload };
 };

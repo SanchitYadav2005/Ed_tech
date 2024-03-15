acdd67778a1f15ef4d28dc9c91fe96907ab8410a
unknown <sanchit2005yudavanshi@gmail.com>
2024-03-15 21:49:46 +0530

    redone multer

--- backend/routes/fileRoutes.js
+++ backend/routes/fileRoutes.js
@@ -6,10 +6,15 @@ const {
   getSingleFile,
   deleteFile,
 } = require("../controllers/fileController");
-// const multer = require("multer");
-// const upload = multer({dest: 'upload/'})
+const multer = require("multer");
+const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 5 * 1024 * 1024 * 1024,
+  },
+});
 
-router.post("/developer/:id/file",  uploadFile);
+router.post("/developer/:id/file", upload.single('file'), uploadFile);
 router.get("/files", getAllFiles);
 router.get("/:id/file", getSingleFile);
 router.delete("/:id/delete", deleteFile);

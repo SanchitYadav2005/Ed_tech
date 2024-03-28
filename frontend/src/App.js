4042c845366d5d4e73fb3c595207e79a8f0f2a45
unknown <sanchit2005yudavanshi@gmail.com>
2024-03-28 20:47:54 +0530

    fixed is developer in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Home from "./pages/Home";
 import Diversion from "./pages/Diversion";
 import Login from "./pages/Login";
@@ -6,21 +6,21 @@ import Signup from "./pages/Signup";
 import FilePage from "./pages/FilePage";
 import Notes from "./pages/Notes";
 import { Routes, Route } from "react-router-dom";
-import { useState } from "react";
 
 function App() {
   const [isDeveloper, toggleDeveloper] = useState(true);
 
   const toggle = () => {
-    toggleDeveloper((prevIsDeveloper) => !prevIsDeveloper);
+    toggleDeveloper((prevState) => !prevState);
   };
+
   return (
     <React.Fragment>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route
           path="/register"
-          element={<Diversion togleDeveloper={toggle} />}
+          element={<Diversion toggleDeveloper={toggle} />}
         />
         <Route
           path="/developer/login"

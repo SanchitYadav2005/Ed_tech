705c34b8509d0f713a1cf846da8d52875dd5cda6
unknown <sanchit2005yudavanshi@gmail.com>
2024-04-01 22:56:12 +0530

    working on videos page

--- frontend/src/pages/Videos.js
+++ frontend/src/pages/Videos.js
@@ -1,9 +1,20 @@
 import PostNav from "../components/PostsNav";
+import axios from 'axios';
+import { useState, useEffect } from "react";
+import { useGetAllLinks } from "../hooks/getAllLinks";
 
 const Videos = () => {
+// const [allData, setAllData] = useState(null);
+const{isLoading, data} = useGetAllLinks();
+
+useEffect(()=>{
+  console.log(data)
+  console.log(isLoading)
+})
   return (
     <>
     <PostNav/>
+
     </>
   );
 };

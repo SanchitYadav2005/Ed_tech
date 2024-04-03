305e4c5cb5104e28cdc044ff0d4d049db24e22e5
unknown <sanchit2005yudavanshi@gmail.com>
2024-04-03 19:54:30 +0530

    done with the loader

--- frontend/src/pages/Videos.js
+++ frontend/src/pages/Videos.js
@@ -1,20 +1,37 @@
 import PostNav from "../components/PostsNav";
-import axios from 'axios';
-import { useState, useEffect } from "react";
+import { MutatingDots } from "react-loader-spinner";
+import { useEffect } from "react";
 import { useGetAllLinks } from "../hooks/getAllLinks";
+import "../styles/videos.scss";
 
 const Videos = () => {
-// const [allData, setAllData] = useState(null);
-const{isLoading, data} = useGetAllLinks();
+  // const [allData, setAllData] = useState(null);
+  const { isLoading, data } = useGetAllLinks();
 
-useEffect(()=>{
-  console.log(data)
-  console.log(isLoading)
-})
+  useEffect(() => {
+    console.log(data?.links?.map((link) => link));
+    console.log(isLoading);
+  });
   return (
     <>
-    <PostNav/>
-
+      <PostNav />
+      {isLoading ? (
+        <div className="mutating-dots">
+          <MutatingDots
+            visible={true}
+            height="100"
+            width="100"
+            color="#f34a23"
+            secondaryColor="#f34a23"
+            radius="12.5"
+            ariaLabel="mutating-dots-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+          />
+        </div>
+      ) : (
+        <h1>{data?.links?.map((link) => link)}</h1>
+      )}
     </>
   );
 };

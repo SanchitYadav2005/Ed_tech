cdd2593448e820723f60cc13b95c503465cb729f
unknown <sanchit2005yudavanshi@gmail.com>
2024-04-05 15:08:33 +0530

    done with the structure of single video component

--- frontend/src/components/SingleVideo.js
+++ frontend/src/components/SingleVideo.js
@@ -1,5 +1,7 @@
-import SingleTag from './SingleTag';
-import "../styles/video.scss"
+import SingleTag from "./SingleTag";
+import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import "../styles/video.scss";
 
 const SingleVideo = ({
   title,
@@ -17,20 +19,23 @@ const SingleVideo = ({
           <img className="card-img" src={thumbnail} alt={title} />
         </a>
         <div className="card-heading">
-          <h3>{title}</h3>
-          <span>{channelName}</span>
+          <h3 className="title">{title}</h3>
+          <span className="channelName">{channelName}</span>
         </div>
         <div className="tags">
-          <SingleTag tags={tags}/>
+          <SingleTag tags={tags} />
+        </div>
+        <div className="counts">
+          <div className="views">
+            <RemoveRedEyeIcon className="eye-icon" />
+            <span className="number">{viewsCount}</span>
+          </div>
+          <div className="likes">
+            <ThumbUpIcon className="thumb-icon" />
+            <span className="number">{likeCount}</span>
+          </div>
         </div>
       </div>
-      {/* <h1>{title}</h1>
-            <h2>{description}</h2>
-            <h3>{channelName}</h3>
-            <img src={thumbnail} alt="thumbnail"/>
-            <h4>{likeCount}</h4>
-            <h5>{viewsCount}</h5>
-            <h1>{link}</h1> */}
     </>
   );
 };

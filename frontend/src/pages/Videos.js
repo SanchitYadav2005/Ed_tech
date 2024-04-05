b99e66f63b9f94191538b8b3e2d77bc41dd63032
unknown <sanchit2005yudavanshi@gmail.com>
2024-04-05 14:02:52 +0530

    video page is done

--- frontend/src/pages/Videos.js
+++ frontend/src/pages/Videos.js
@@ -1,51 +1,54 @@
 import PostNav from "../components/PostsNav";
 import axios from "axios";
-import { MutatingDots } from "react-loader-spinner";
-import { useState,useEffect } from "react";
+import Loader from "../components/Loader";
+import { useState, useEffect } from "react";
 import { useGetAllLinks } from "../hooks/getAllLinks";
-import "../styles/videos.scss";
+import SingleVideo from "../components/SingleVideo";
 
 const Videos = () => {
   const [videoData, setVideoData] = useState([]);
-const { isLoading, data } = useGetAllLinks();
-const videoIds = data?.videoIds;
+  const { isLoading, data } = useGetAllLinks();
+  const videoIds = data?.videoIds;
 
-useEffect(() => {
+  useEffect(() => {
     const getVideoData = async () => {
-        if (videoIds && videoIds.length > 0) {
-            const promises = videoIds.map(async (id) => {
-                const res = await axios.get(`https://www.googleapis.com/youtube/v3/videos?id=${id}&key=AIzaSyBad8gYJi8K3_2iR2QP1BQOFqXR-v2-tTM&part=snippet,contentDetails,statistics`);
-                return res.data;
-            });
-            const responseData = await Promise.all(promises);
-            setVideoData(responseData);
-        }
-    }
+      if (videoIds && videoIds.length > 0) {
+        const promises = videoIds.map(async (id) => {
+          const res = await axios.get(
+            `https://www.googleapis.com/youtube/v3/videos?id=${id}&key=AIzaSyBad8gYJi8K3_2iR2QP1BQOFqXR-v2-tTM&part=snippet,contentDetails,statistics`
+          );
+          return res.data;
+        });
+        const responseData = await Promise.all(promises);
+        setVideoData(responseData);
+      }
+    };
     getVideoData();
-}, [videoIds]);
-
-console.log(videoData);
+  }, [videoIds]);
+  const link = data?.links?.map((link) => link);
+  console.log(videoData);
 
   return (
     <>
       <PostNav />
       {isLoading ? (
-        <div className="mutating-dots">
-          <MutatingDots
-            visible={true}
-            height="100"
-            width="100"
-            color="#f34a23"
-            secondaryColor="#f34a23"
-            radius="12.5"
-            ariaLabel="mutating-dots-loading"
-            wrapperStyle={{}}
-            wrapperClass=""
-          />
-        </div>
+        <Loader />
       ) : (
-        <h1>{data?.links?.map((link) => link)}</h1>
-        
+        <>
+          <h2>
+            {videoData?.map((video) => (
+              <SingleVideo
+                title={video.items[0].snippet.title}
+                description={video.items[0].snippet.description}
+                channelName={video.items[0].snippet.channelTitle}
+                thumbnail={video.items[0].snippet.thumbnails.default.url}
+                likeCount={video.items[0].statistics.likeCount}
+                viewsCount={video.items[0].statistics.viewCount}
+                link={link}
+              />
+            ))}
+          </h2>
+        </>
       )}
     </>
   );

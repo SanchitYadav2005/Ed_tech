9297f2473eb1a044af236a0fc67c9318f07c9ea5
unknown <sanchit2005yudavanshi@gmail.com>
2024-04-05 14:36:35 +0530

    working on single video compoenent

--- frontend/src/components/SingleVideo.js
+++ frontend/src/components/SingleVideo.js
@@ -1,15 +1,35 @@
-const SingleVideo = ({title, description, channelName, thumbnail, likeCount, viewsCount, link}) => {
-    return ( 
-        <>
-            <h1>{title}</h1>
+const SingleVideo = ({
+  title,
+  tag,
+  channelName,
+  thumbnail,
+  likeCount,
+  viewsCount,
+  link,
+}) => {
+  return (
+    <>
+      <div className="card">
+        <a href={link} className="video-link">
+          <img className="card-img" src={thumbnail} alt={title} />
+        </a>
+        <div className="card-heading">
+          <h3>{title}</h3>
+          <span>{channelName}</span>
+        </div>
+        <div className="description">
+          <p>{tag}</p>
+        </div>
+      </div>
+      {/* <h1>{title}</h1>
             <h2>{description}</h2>
             <h3>{channelName}</h3>
             <img src={thumbnail} alt="thumbnail"/>
             <h4>{likeCount}</h4>
             <h5>{viewsCount}</h5>
-            <h1>{link}</h1>
-        </>
-     );
-}
- 
+            <h1>{link}</h1> */}
+    </>
+  );
+};
+
 export default SingleVideo;

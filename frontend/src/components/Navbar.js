e8d5df487c22d8778a737ae318c2a7a8875291f4
unknown <sanchit2005yudavanshi@gmail.com>
2024-04-09 14:58:34 +0530

    working on how to display register and logout dynamically

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,7 +1,14 @@
 import "../styles/navbar.scss";
 import { Link } from "react-router-dom";
+import useAuthContext from "../hooks/useAuthContext";
+import { useLogOut } from "../hooks/useLogout";
 
 function Navbar() {
+  const { user } = useAuthContext();
+  const { logout } = useLogOut();
+  const handleClick = () => {
+    logout();
+  };
   return (
     <>
       <nav className="navbar">
@@ -13,8 +20,8 @@ function Navbar() {
             </a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/contact">
-              Contact
+            <a className="nav-link" href="/videos">
+              Videos
             </a>
           </li>
           <li className="nav-item">
@@ -23,9 +30,13 @@ function Navbar() {
             </a>
           </li>
         </ul>
-        <Link to="/register" className="btn">
-          Register
-        </Link>
+        {user ? (
+          <Link onClick={handleClick}>logout</Link>
+        ) : (
+          <Link to="/register" className="btn">
+            Register
+          </Link>
+        )}
       </nav>
     </>
   );

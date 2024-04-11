06811014bc645013bbe7433cec0465c3523d7872
unknown <sanchit2005yudavanshi@gmail.com>
2024-04-11 11:36:00 +0530

    fixed error in single post

--- frontend/src/components/SinglePost.js
+++ frontend/src/components/SinglePost.js
@@ -1,32 +1,24 @@
-import "../styles/post.scss";
+import '../styles/post.scss'
 
 const SinglePost = ({ base64, id }) => {
-  if (base64) {
-    return (
-      <>
-        <object
-          data={`data:application/pdf;base64,${base64}`}
+  return (
+    <>
+      <object
+        data={`data:application/pdf;base64,${base64}`}
+        type="application/pdf"
+        width="100%"
+        height="600px"
+        key={id}
+        className="file-object"
+      >
+        <embed
+          src={`data:application/pdf;base64,${base64}`}
           type="application/pdf"
-          width="100%"
-          height="600px"
-          key={id}
-          className="file-object"
-        >
-          <embed
-            src={`data:application/pdf;base64,${base64}`}
-            type="application/pdf"
-            className="embed-file"
-          />
-        </object>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <span className="error">No post available!</span>
-      </>
-    );
-  }
+          className="embed-file"
+        />
+      </object>
+    </>
+  );
 };
 
 export default SinglePost;

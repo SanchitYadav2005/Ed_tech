849fa8f07403574ea1b8c8ff2bf5133fe35325f4
unknown <sanchit2005yudavanshi@gmail.com>
2024-04-11 11:36:47 +0530

    routes are protected

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Home from "./pages/Home";
 import Diversion from "./pages/Diversion";
 import Login from "./pages/Login";
@@ -6,14 +6,26 @@ import Signup from "./pages/Signup";
 import FilePage from "./pages/FilePage";
 import Notes from "./pages/Notes";
 import Videos from "./pages/Videos";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   const [isDeveloper, toggleDeveloper] = useState(true);
-
+  const [token, setToken] = useState(null);
   const toggle = () => {
     toggleDeveloper((prevState) => !prevState);
   };
+  const getToken = () => {
+    const storedToken = localStorage.getItem("token");
+    if (storedToken) {
+      setToken(storedToken);
+    } else {
+      console.log("No token found!");
+    }
+  };
+
+  useEffect(() => {
+    getToken();
+  });
 
   return (
     <React.Fragment>
@@ -33,9 +45,18 @@ function App() {
           element={<Signup isDeveloper={isDeveloper} />}
         />
         <Route path="/learner/signup" element={<Signup />} />
-        <Route path={`/developer/:id`} element={<FilePage />} />
-        <Route path="/notes" element={<Notes />} />
-        <Route path="/videos" element={<Videos />} />
+        <Route
+          path={`/developer/:id`}
+          element={token ? <FilePage /> : <Navigate to={"/register"} />}
+        />
+        <Route
+          path="/notes"
+          element={token ? <Notes /> : <Navigate to={"/register"} />}
+        />
+        <Route
+          path="/videos"
+          element={token ? <Videos /> : <Navigate to={"/register"} />}
+        />
       </Routes>
     </React.Fragment>
   );

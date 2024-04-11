6175836ad2daa8d8d62fbdab2669294f2aeb4af8
unknown <sanchit2005yudavanshi@gmail.com>
2024-04-11 10:54:34 +0530

    added error handling in single post component

--- frontend/src/components/SinglePost.js
+++ frontend/src/components/SinglePost.js
@@ -1,24 +1,32 @@
-import '../styles/post.scss'
+import "../styles/post.scss";
 
 const SinglePost = ({ base64, id }) => {
-  return (
-    <>
-      <object
-        data={`data:application/pdf;base64,${base64}`}
-        type="application/pdf"
-        width="100%"
-        height="600px"
-        key={id}
-        className="file-object"
-      >
-        <embed
-          src={`data:application/pdf;base64,${base64}`}
+  if (base64) {
+    return (
+      <>
+        <object
+          data={`data:application/pdf;base64,${base64}`}
           type="application/pdf"
-          className="embed-file"
-        />
-      </object>
-    </>
-  );
+          width="100%"
+          height="600px"
+          key={id}
+          className="file-object"
+        >
+          <embed
+            src={`data:application/pdf;base64,${base64}`}
+            type="application/pdf"
+            className="embed-file"
+          />
+        </object>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <span className="error">No post available!</span>
+      </>
+    );
+  }
 };
 
 export default SinglePost;
